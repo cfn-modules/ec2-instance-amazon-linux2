1f9832ee7ed43cd133174e0bf9fa442dfd704aa9
Michael Wittig <michael@widdix.de>
2019-11-05 13:09:49 +0100

    update test dependencies (#15)

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
 const test = require('ava');
 const cfntest = require('@cfn-modules/test');
-const axios = require('axios');
 
 test.serial('defaults', async t => {
   const stackName = cfntest.stackName();
@@ -51,7 +50,7 @@ test.serial('user-data-ingress', async t => {
     t.log(await cfntest.createStack(`${__dirname}/user-data-ingress.yml`, stackName, {}));
     const outputs = await cfntest.getStackOutputs(stackName);
     t.log(outputs);
-    const res = await axios.get(`http://${outputs.PublicIpAddress}`);
+    const res = await cfntest.probeHttpGet(`http://${outputs.PublicIpAddress}`);
     t.is(res.status, 200);
   } finally {
     t.log(await cfntest.deleteStack(stackName));
